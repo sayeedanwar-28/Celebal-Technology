836df7cfb88b507020d8370ec85715b2e6df87ac
Sayeed Anwar <121096649+sayeedanwar-28@users.noreply.github.com>
2024-06-04 22:43:41 +0530

    Add files via upload

---
+++ week 1/lowert.py
@@ -0,0 +1,9 @@
+# To print lower triangle using '*'
+
+n = int(input("Enter the number of rows: "))
+
+for i in range(n):
+    for j in range(i+1):
+        print("*", end="")
+    print()
+     
---
+++ week 1/triangle.py
@@ -0,0 +1,9 @@
+# Create  pyramid containing the "*" character
+n = int(input("Enter the number of rows: "))
+
+for i in range(n):
+    for j in range(n-i-1):
+        print(" ", end="")
+    for j in range(2*i+1):
+        print("*", end="")
+    print()
